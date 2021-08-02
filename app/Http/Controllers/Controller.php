00617c2bb85cbfc6a6ddfd80829137dc1c4e3fc1
KaiHelsen <kai@Helsen.info>
2021-08-02 15:30:06 +0200

    completed three main tasks: welcome page displays 'hello world!', simple data is stored and extracted from cache, and simple migration has been created and tested.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,33 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Cache;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        //DONE: 1) Display Hello world? done in blade, easy adjustment
+        //DONE: 2) store something in redis cache, and extract from cache
+        //DONE: 3) create mySQL migration of basic user model (name, email, etc.)
+
+        $this->TestCaching();
+        return view('welcome',);
+    }
+
+    private function TestCaching(): void
+    {
+        $value = Cache::get('testKey');
+        if ($value === null)
+        {
+            echo("nothing in the cache as of yet. Rectifying...");
+            Cache::put('testKey', 'SomeResponse', 60);
+        }
+        else
+        {
+            echo $value;
+        }
+    }
 }
---
+++ database/migrations/2021_08_02_124121_create_useraccount_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userAccount', function (Blueprint $table) {
+            $table->id();
+            $table->string('userName');
+            $table->string('email');
+            $table->string('phoneNumber');
+            $table->string('address');
+            $table->unsignedMediumInteger('houseNumber');
+            $table->string('zipCode');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('useraccount');
+    }
+}
